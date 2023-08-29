11e5f82ed45fda23c8c891b6eabdf95f7ae9c491
MohsenSami <mohsensami88@gmail.com>
2023-08-29 21:37:09 +0330

    useMutation for delet and update posts

--- blog-em/src/PostDetail.jsx
+++ blog-em/src/PostDetail.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query';
+import { useQuery, useMutation } from 'react-query';
 
 async function fetchComments(postId) {
     const response = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`);
@@ -21,12 +21,21 @@ async function updatePost(postId) {
 export function PostDetail({ post }) {
     // replace with useQuery
     const { data, isLoading, isError, error } = useQuery(['comments', post.id], () => fetchComments(post.id));
+    const deleteMutation = useMutation((postId) => deletePost(postId));
+    const updateMutation = useMutation((postId) => updatePost(postId));
     if (isLoading) return <h3>Loading ...</h3>;
     if (isError) return <h3>{error.toString()}</h3>;
     return (
         <>
             <h3 style={{ color: 'blue' }}>{post.title}</h3>
-            <button>Delete</button> <button>Update title</button>
+            <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+            {deleteMutation.isLoading && <p> deleting the post</p>}
+            {deleteMutation.isError && <p>Error deleting post</p>}
+            {deleteMutation.isSuccess && <p>post has been deleted</p>}
+            {updateMutation.isLoading && <p> Updating the post</p>}
+            {updateMutation.isError && <p>Error Updating post</p>}
+            {updateMutation.isSuccess && <p>post has been updated</p>}
+            <button onClick={() => updateMutation.mutate(post.id)}>Update title</button>
             <p>{post.body}</p>
             <h4>Comments</h4>
             {data.map((comment) => (
